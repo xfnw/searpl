c62b48ae1f52c3f1529ed2a41d3f35fe705f3b21
xfnw <xfnw@thunix.net>
2020-12-14 17:22:28 -0500

    unique urls

--- urls.sh
+++ urls.sh
@@ -1,3 +1,3 @@
-wget --spider --force-html -r -l2 -H $@ 2>&1 \
+wget --spider --force-html -r -l1 -H $@ 2>&1 \
   | grep '^--' | awk '{ print $3 }' \
-  | grep -v '\.\(css\|js\|png\|gif\|jpg\)$'
+  | grep -v '\.\(css\|js\|png\|gif\|jpg\|txt\|ico\|ttf\)$'
