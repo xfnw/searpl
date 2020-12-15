2b46e8899fdf08b6c67275ef152c1f15b5ea020d
xfnw <xfnw@thunix.net>
2020-12-15 10:17:28 -0500

    set a useragent

--- urls.sh
+++ urls.sh
@@ -1,3 +1,9 @@
-wget --spider --force-html -r -l1 -H $@ 2>&1 \
-  | grep '^--' | awk '{ print $3 }' \
-  | grep -v '\.\(css\|js\|png\|gif\|jpg\|txt\|ico\|ttf\|svg\)$'
+wget --spider --force-html -r -l1 -H -U 'searplbot/1.0' $@ 2>&1 | tee wg
+
+grep '^--' wg | awk '{ print $3 }' \
+  | grep -v '\.\(css\|js\|png\|gif\|jpg\|txt\|ico\|ttf\|svg\)$' \
+  | tee ur
+
+sleep 10
+
+php crawl.php $(cat ur)
